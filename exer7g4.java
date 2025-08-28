510ed26bdf4cf72253c0cf3d3b80dc353918e06b
josircalica14 <yowsirrr@gmail.com>
2025-08-28 20:59:02 +0800

    Added the code file

---
+++ exer7g4.java
@@ -0,0 +1,10 @@
+public class exer7g4{
+
+     public static void main(String []args){
+        System.out.println("Exercise 7");
+        System.out.println("Group 4 - Queuing QR Code");
+        System.out.println("______________________________");
+        System.out.println("Members");
+        System.out.println("1. Josir James S. Calica");
+     }
+}
